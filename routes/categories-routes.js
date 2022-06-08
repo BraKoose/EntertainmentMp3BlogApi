d58b345a7b5988099d051d1e9abb1e0157f54433
Koose <KooseTheBlogger@gmail.com>
2022-06-08 14:49:12 +0000

    comments done , yet to profiles

--- routes/categories-routes.js
+++ routes/categories-routes.js
@@ -3,35 +3,22 @@ const router = require("express").Router();
 const { ensureAuthenticated, ensureAuthorized } = require("../middleware/auth-middleware");
 
 const { addOne,
-    removeOne,
-    updateOne,
-    getAll,
-    getOne } = require("../controllers/categories-controller")
+    removeOne } = require("../controllers/comments-controller")
 
 
-router.get("/categories", async (req, res) => {
-    await getAll(req, res);
+
+
+router.post("/comment", ensureAuthenticated, async (req, res) => {
+    await addOne(req, res);
 });
 
-router.post("/categories", ensureAuthenticated,
-    ensureAuthorized(["admin"]), async (req, res) => {
-        await addOne(req, res);
-    });
 
 
-router.put("/categories/:id", ensureAuthenticated,
-    ensureAuthorized(["admin"]), async (req, res) => {
-        await updateOne(req, res);
-    });
 
-router.get("/categories/:id", async (req, res) => {
-    await getOne(req, res);
+
+router.delete("/categories/:id", ensureAuthenticated, async (req, res) => {
+    await removeOne(req, res);
 });
-
-router.delete("/categories/:id", ensureAuthenticated,
-    ensureAuthorized(["admin"]), async (req, res) => {
-        await removeOne(req, res);
-    });
 
 
 module.exports = router;
