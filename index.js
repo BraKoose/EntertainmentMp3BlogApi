6c0c4fefaa6a499099a16aa565118dba1d2a349f
Koose <KooseTheBlogger@gmail.com>
2022-06-10 11:02:31 +0000

    all routers done yet to be tested

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const passport = require("passport");
 const { connect } = require("mongoose");
 
 const app = express();
+const router = require("./routes/index");
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(passport.initialize());
-
+app.use("./middleware/passport-middleware.js")(passport)
 app.use(paginate.middleware(process.env.LIMIT, process.env.MAX_LIMIT));
+
+app.use(router);
+
 
 const runApp = async () => {
     try {
