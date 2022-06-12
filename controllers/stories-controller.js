758ff7dd37d16a2dfd8eb480a3be95be35a17e76
Koose <KooseTheBlogger@gmail.com>
2022-06-12 07:47:53 +0000

    swagger done slug, imageUrl upload, so the full story object works

--- controllers/stories-controller.js
+++ controllers/stories-controller.js
@@ -3,11 +3,15 @@ const paginate = require("express-paginate");
 const Comment = require("../models/comment");
 
 const addOne = async (req, res) => {
+    const newRecord = new Story({
+        ...req.body,
+        createdBy: req.user._id,
+    });
+
     try {
-        const newRecord = new Story({
-            ...req.body,
-            createdBy: req.user._id,
-        });
+        if (!newRecord.slug) {
+            newRecord.slug = generateSlug(newRecord.title);
+        }
         await newRecord.save();
         return res.status(201).json({
             message: "Item Succesfully created",
@@ -157,6 +161,17 @@ const getOneBySlug = async (req, res) => {
     }
 };
 
+const generateSlug = (title) => {
+    const slugText = title.toString()
+        .trim()
+        .toLowerCase()
+        .replace(/\s+/g, "-")
+        .replace(/[^\w\-]+/g, "")
+        .replace(/\-\-+/g, "-")
+        .replace(/^-+/, "")
+        .replace(/-+$/, "");
+    return slugText;
+}
 
 
 module.exports = {
--- routes/videos-routes.js
+++ routes/videos-routes.js
@@ -11,30 +11,58 @@ const { addOne,
 
 
 router.get("/videos", async (req, res) => {
+    // #swagger.tags = ['Posts']
+
     await getAll(req, res);
 });
 
 router.post("/videos", ensureAuthenticated,
     ensureAuthorized(["admin"]), async (req, res) => {
+        /*  #swagger.tags = ['Posts']
+       #swagger.security = [{
+       "Authorization": []
+       }]
+       #swagger.parameters['obj'] = {
+           in: 'body',
+           required: true,
+           schema: { $ref: "#/definitions/VideoModel" }
+   } */
         await addOne(req, res);
     });
 
 
 router.put("/videos/:id", ensureAuthenticated,
     ensureAuthorized(["admin"]), async (req, res) => {
+        /*  #swagger.tags = ['Posts']
+       #swagger.security = [{
+       "Authorization": []
+       }]
+       #swagger.parameters['obj'] = {
+           in: 'body',
+           required: true,
+           schema: { $ref: "#/definitions/VideoModel" }
+   } */s
         await updateOne(req, res);
     });
 
 router.get("/videos/:id", async (req, res) => {
+    // #swagger.tags = ['Posts']  
     await getOne(req, res);
 });
 
 router.delete("/videos/:id", ensureAuthenticated,
     ensureAuthorized(["admin"]), async (req, res) => {
+        /*  #swagger.tags = ['Posts']
+      #swagger.security = [{
+      "Authorization": []
+      }]
+  */
         await removeOne(req, res);
     });
 
 router.get("/videos/top", async (req, res) => {
+    // #swagger.tags = ['Posts']
+
     await getTopvideos(req, res);
 });
 
